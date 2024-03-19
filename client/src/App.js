199482f17715dc7821de056206fd7f8ccf65d902
vrk0ranit <ranitbag49@gmail.com>
2024-03-19 11:06:34 +0530

    fipcart

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+//components
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/header/Search.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {InputBase, Box, styled} from '@mui/material'
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+
+const SearchContainer = styled(Box)`
+background: #fff;
+width: 38%;
+broder-radius: 2px;
+margin-left: 10px;
+display: flex;
+`;
+const InputSearchBase = styled(InputBase)`
+ padding-left: 20px;
+ width: 100%;
+ font-size: unset;
+`;
+
+const SearchIconWrapper = styled(Box)`
+ color: blue;
+ padding: 5px;
+`
+
+const Search = () => {
+  return (
+     <SearchContainer>
+         <InputSearchBase
+          placeholder='Search for products brands and more'
+         />
+         <SearchIconWrapper>
+            <ManageSearchIcon/>
+         </SearchIconWrapper>
+     </SearchContainer>
+  )
+}
+
+export default Search
