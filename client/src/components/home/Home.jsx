5ea7f9cbe27bdb3f3964dd93aabccb4c03672bf3
vrk0ranit <ranitbag49@gmail.com>
2024-03-21 17:37:46 +0530

    redux setup

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{ useEffect } from 'react'
 
 //components
 
@@ -7,12 +7,25 @@ import Banner from './Banner'
 
 import { Box ,styled } from '@mui/material'
 
+import { getProducts } from '../../redux/actions/productActions'
+import { useDispatch,useSelector } from 'react-redux'
+
 const Component = styled(Box)`
  padding: 10px;
  background: #f2f2f2;
 `
 
 const Home = () => {
+
+   useSelector(state => state.getProducts)
+  
+
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+     dispatch(getProducts())
+  },[dispatch])
+
   return (
     <>
       <NavBar/>
