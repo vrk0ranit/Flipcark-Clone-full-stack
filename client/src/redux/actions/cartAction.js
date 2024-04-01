c54c36785571087e551c4da7b1dc225a19c510c8
Ranit Bag <118438283+vrk0ranit@users.noreply.github.com>
2024-04-01 18:42:46 +0530

    Update cartAction.js

--- client/src/redux/actions/cartAction.js
+++ client/src/redux/actions/cartAction.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as actionType from '../constants/cartConstant'
 
-const URL = `http://localhost:8000`
+const URL = `http://mini-flipcark-clone-api.vercel.app`
 
 export const addToCart = (id, quantity) => async(dispatch) => {
    try {
